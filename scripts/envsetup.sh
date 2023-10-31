caf04519e477f3f5e4544577187b0be68faff14e
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2023-10-31 12:36:12 +0530

    Update envsetup.sh

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+sudo apt install python3.10-venv
 
+if [ -d "env" ] 
+then
+    echo "Python virtual environment exists." 
+else
+    python3.10 -m venv env
+fi
+
+source env/bin/activate
+
+
+pip3 install -r requirements.txt
+
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
