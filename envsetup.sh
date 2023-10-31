789d628b6d4bf79be47fa3fa18ca81ef98dba64c
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2023-10-31 15:05:38 +0530

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 sudo apt install python3.10-venv
+# Add this line after each command
+set -e
 
 if [ -d "env" ] 
 then
     echo "Python virtual environment exists." 
 else
-    python3.10 -m venv env
+    python3.10 -m venv env || { echo 'Failed to create virtual environment.'; exit 1; }
+
 fi
 
 source env/bin/activate
