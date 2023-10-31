ffd3a6a78344b17e1889fd2b8206ad7153557197
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2023-10-31 22:27:06 +0530

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,15 +2,14 @@
 
 source /var/lib/jenkins/workspace/paac/env/bin/activate
 
-cd /var/lib/jenkins/workspace/django-cicd/app
-
+cd /var/lib/jenkins/workspace/paac/app
 python3.10 manage.py makemigrations
 python3.10 manage.py migrate
 python3.10 manage.py collectstatic -- no-input
 
 echo "Migrations done"
 
-cd /var/lib/jenkins/workspace/django-cicd
+cd /var/lib/jenkins/workspace/paac
 
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
