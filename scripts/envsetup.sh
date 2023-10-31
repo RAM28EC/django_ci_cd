37a24fb19cef2846e54a2e6768901d285af9878a
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2023-10-31 12:31:49 +0530

    Update envsetup.sh

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -4,11 +4,12 @@ if [ -d "env" ]
 then
     echo "Python virtual environment exists." 
 else
-    python3 -m venv env
+     python3.10 -m venv env
 fi
 
 source env/bin/activate
 
+soudo apt install python3-pip -y
 
 pip3 install -r requirements.txt
 
