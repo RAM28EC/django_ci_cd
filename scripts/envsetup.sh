216adb1f52dbdac7826423f6598a38894245d7d0
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2023-10-31 11:17:59 +0530

    Update envsetup.sh

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-if [ -d "env" ] 
-then
-    echo "Python virtual environment exists." 
+# Check if running as superuser
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as superuser (with sudo)."
+    exit 1
+fi
+
+# Check if virtual environment exists
+if [ -d "env" ]; then
+    echo "Python virtual environment exists."
 else
+    # Try to install python3-venv package if not installed
+    apt-get update
+    apt-get install -y python3-venv
     python3 -m venv env
 fi
 
+# Activate virtual environment
 source env/bin/activate
 
-
+# Install dependencies
 pip3 install -r requirements.txt
 
-if [ -d "logs" ] 
-then
-    echo "Log folder exists." 
+# Check and create log folder
+if [ -d "logs" ]; then
+    echo "Log folder exists."
 else
     mkdir logs
     touch logs/error.log logs/access.log
 fi
 
-sudo chmod -R 777 logs
+# Ensure proper permissions on log folder
+chmod -R 777 logs
