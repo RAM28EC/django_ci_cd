5ef6985b17c930cdf79c17dfff569c3c50fa034d
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2023-10-31 15:07:33 +0530

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-sudo apt install python3.10-venv
-# Add this line after each command
+
+# Add error handling
 set -e
+
+echo "Current Working Directory: $(pwd)"
+echo "PATH: $PATH"
+
+sudo apt install python3.10-venv
 
 if [ -d "env" ] 
 then
     echo "Python virtual environment exists." 
 else
     python3.10 -m venv env || { echo 'Failed to create virtual environment.'; exit 1; }
-
 fi
 
-source env/bin/activate
+# Print the contents of the directory to verify the existence of env
+ls -l
 
+# Print the contents of env to see if activate is present
+ls -l env
+
+source env/bin/activate
 
 pip3 install -r requirements.txt
 
