b90335458c148482d2ef9e4a88b5f5ebad38bde7
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2023-11-01 13:46:32 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@ sudo apt install nginx -y
 sudo cp -rf app.conf /etc/nginx/sites-available/app
 chmod 710 /var/lib/jenkins/workspace/paac
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+#sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
