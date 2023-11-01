e01cf6da40a206570e991f7edacf7dd02cc68f7c
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2023-11-01 15:42:42 +0530

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/lib/jenkins/workspace/paac/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 # Default primary key field type
